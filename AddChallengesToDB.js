f7fb51d7686d500dfd82e5fac128b32635b29fe3
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-22 18:41:11 +0000

    added txt files and wrote program to parse and add to database

--- AddChallengesToDB.js
+++ AddChallengesToDB.js
@@ -6,17 +6,19 @@ const db = new Database();
 
 // File patterns for each round
 const filePatterns = {
-  1: glob.glob.sync("path_to_files/swapsclean_4_steps_*_difference.txt").concat(glob.glob.sync("path_to_files/swapsclean_5_steps_*_difference.txt")),
-  2: glob.glob.sync("path_to_files/swapsclean_4_steps_*_difference.txt").concat(glob.glob.sync("path_to_files/swapsclean_5_steps_*_difference.txt")),
-  3: glob.glob.sync("path_to_files/swapsclean_5_steps_*_difference.txt"),
-  4: glob.glob.sync("path_to_files/swapsclean_5_steps_*_difference.txt").concat(glob.glob.sync("path_to_files/swapsclean_6_steps_*_difference.txt")),
-  5: glob.glob.sync("path_to_files/swapsclean_6_steps_*_difference.txt").concat(glob.glob.sync("path_to_files/swapsclean_7_steps_*_difference.txt")).concat(glob.glob.sync("path_to_files/swapsclean_8_steps_*_difference.txt")),
+  1: glob.glob.sync("wordpairtxts/swapsclean_4_steps_*_difference.txt").concat(glob.glob.sync("wordpairtxts/swapsclean_5_steps_*_difference.txt")),
+  2: glob.glob.sync("wordpairtxts/swapsclean_4_steps_*_difference.txt").concat(glob.glob.sync("wordpairtxts/swapsclean_5_steps_*_difference.txt")),
+  3: glob.glob.sync("wordpairtxts/swapsclean_5_steps_*_difference.txt"),
+  4: glob.glob.sync("wordpairtxts/swapsclean_5_steps_*_difference.txt").concat(glob.glob.sync("wordpairtxts/swapsclean_6_steps_*_difference.txt")),
+  5: glob.glob.sync("wordpairtxts/swapsclean_6_steps_*_difference.txt").concat(glob.glob.sync("wordpairtxts/swapsclean_7_steps_*_difference.txt")).concat(glob.glob.sync("wordpairtxts/swapsclean_8_steps_*_difference.txt")),
 };
 
 // Function to parse file to word pairs
 function parseTxtToJson(txtFile, seenPairs) {
   let data = fs.readFileSync(txtFile, 'utf8');
   let lines = data.split('\n');
+  
+  let newPairs = [];
 
   for (let line of lines) {
     line = line.trim();
@@ -26,15 +28,19 @@ function parseTxtToJson(txtFile, seenPairs) {
     if (seenPairs.has(`${start}-${goalWord}`)) continue; // Skip if we've seen this pair before
 
     seenPairs.add(`${start}-${goalWord}`);
+    newPairs.push(`${start}-${goalWord}`);
   }
 
-  return Array.from(seenPairs);
+  return newPairs;
 }
 
 async function main() {
   let seenPairs = new Set();
   let version = 1; // Change this for different versions
   let startingDate = new Date('2023-01-01'); // Set your starting date here
+  let completeDays = 0;
+  let startDateStr = formatDate(startingDate);
+  let endDateStr = "";
 
   // Process each round
   for (let round = 1; round <= 5; round++) {
@@ -62,10 +68,14 @@ async function main() {
         if (round === 5) {
           startingDate.setDate(startingDate.getDate() + 1);
           round = 0; // Will be incremented to 1 at the start of the next loop
+          completeDays++;
         }
       }
     }
   }
+  endDateStr = formatDate(startingDate);
+
+  console.log(`Processed ${completeDays} complete 5-round days from ${startDateStr} to ${endDateStr}`);
 }
 
 function formatDate(date) {
