fdb5bd381e31fe7c0999be2ef5fecccf448cf8a4
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-06-23 18:21:14 +0000

    backtrack to last night

--- index.js
+++ index.js
@@ -49,27 +49,6 @@ app.get('/leaderboard', async (req, res) => {
   }
 });
 
-app.get('/wordpairs', async (req, res) => {
-  try {
-    const { version, date, round } = req.query;
-
-    // Perform any necessary validation on the input parameters
-
-    // Fetch the word pair from the database based on version, date, and round
-    const wordPair = await db.get(`version${version}-${date}-round${round}`);
-
-    if (!wordPair) {
-      return res.status(404).json({ message: 'Word pair not found' });
-    }
-
-    const { start_word, goal_word } = wordPair;
-    return res.status(200).json({ start_word, goal_word });
-  } catch (error) {
-    console.error('Error fetching word pair:', error);
-    return res.status(500).json({ message: 'Failed to fetch word pair' });
-  }
-});
-
 app.get('/definition/:word', async (req, res) => {
   try {
     const { word } = req.params;
@@ -97,9 +76,6 @@ app.get('/pronunciation/:word', async (req, res) => {
     return res.status(500).json({ message: 'Failed to fetch pronunciation' });
   }
 });
-
-
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
