40822c0725afe75bac77d1ddb8ca3f8875b8cd72
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-08-25 15:42:33 +0000

    removed comments

--- deletekeys.js
+++ deletekeys.js
@@ -1,28 +1,25 @@
 const Database = require("@replit/database");
 
-// Create a new instance of the database
+
 const db = new Database();
 
-// Define your criteria (e.g., delete entries older than a certain date)
+
 const targetDate = new Date("2024-06-23");
 
-// Retrieve all keys from the database
+
 db.list().then(keys => {
-  // Filter keys based on your criteria
+
   const filteredKeys = keys.filter(key => {
-    // Get the value associated with each key
+
     return db.get(key).then(value => {
-      // Check if the value meets your criteria (e.g., based on date or version)
-      // Modify the condition based on your specific requirements
-      const entryDate = new Date(value.date); // Assuming the entry has a "date" property
+
+      const entryDate = new Date(value.date); 
       return entryDate < targetDate;
     });
   });
 
-  // Delete the filtered keys
   const deletePromises = filteredKeys.map(key => db.delete(key));
 
-  // Wait for all delete operations to complete
   Promise.all(deletePromises)
     .then(() => {
       console.log("Selected entries have been deleted from the database.");
