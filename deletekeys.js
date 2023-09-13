f0aa51548f210e58ac20d2ec9c751897c0ff90fd
Lukewhite9 <134559094+Lukewhite9@users.noreply.github.com>
2023-09-13 18:41:23 +0000

    added new function to parse and add challenges that include the best possible path

--- deletekeys.js
+++ deletekeys.js
@@ -1,33 +1,27 @@
 const Database = require("@replit/database");
-
-
 const db = new Database();
+const targetDate = new Date("2024-09-12");
 
+// Fetch all keys from the database
+db.list()
+  .then(async (keys) => {
+    // Fetch values for all keys and filter based on date and version
+    const valuesPromises = keys.map((key) => db.get(key));
+    const allValues = await Promise.all(valuesPromises);
 
-const targetDate = new Date("2024-06-23");
-
-
-db.list().then(keys => {
-
-  const filteredKeys = keys.filter(key => {
-
-    return db.get(key).then(value => {
-
-      const entryDate = new Date(value.date); 
-      return entryDate < targetDate;
+    const filteredKeys = keys.filter((key, index) => {
+      const entry = allValues[index];
+      const entryDate = new Date(entry.date);
+      return entryDate > targetDate && entry.gameID && entry.gameID.startsWith("version4");
     });
+
+    // Delete entries with filtered keys
+    const deletePromises = filteredKeys.map((key) => db.delete(key));
+    return Promise.all(deletePromises);
+  })
+  .then(() => {
+    console.log("Selected entries have been deleted from the database.");
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
   });
-
-  const deletePromises = filteredKeys.map(key => db.delete(key));
-
-  Promise.all(deletePromises)
-    .then(() => {
-      console.log("Selected entries have been deleted from the database.");
-    })
-    .catch(error => {
-      console.error("An error occurred while deleting the entries:", error);
-    });
-})
-.catch(error => {
-  console.error("An error occurred while listing the keys:", error);
-});
